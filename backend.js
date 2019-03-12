30514c87938aae6afeb16eea29608a8f27706c05
Tatiana <choisy.tatiana@gmail.com>
2019-03-12 11:58:58 +0100

    first commit

--- backend.js
+++ backend.js
@@ -1,39 +1,167 @@
-// je vais chercher le driver sqlite3 dans node_modules
 const sqlite3 = require("sqlite3").verbose(); // verbose décris les erreures
 const express = require("express"); // express pour créé les routes
-const dbFile = "test.db"; // créé ficher db qui sera la base de donnée
+const dbFile = "testroutes.db"; // créé ficher db qui sera la base de donnée
 const db = new sqlite3.Database(dbFile); // rempli base de donné
 const cors = require("cors"); // autorise le partage d'info
 const app = express();
+const bodyParser = require("body-parser");
+
+
+
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 db.serialize(() => {
-  //effectu une action et attend la fin de cette action avant la suite du code
+  //effectu une Action et attend la fin de cette Action avant la suite du code
   db.run(
-    "CREATE TABLE IF NOT EXISTS anime (id INTEGER PRIMARY KEY AUTOINCREMENT, nom VARCHAR(50), synopsis VARCHAR(500), auteur VARCHAR(50), genre VARCHAR(50), type VARCHAR(50), date_ajout VARCHAR(50), date_sorti VARCHAR(50), duree VARCHAR(50), status VARCHAR(50))"
+    "CREATE TABLE IF NOT EXISTS categories (category_id INTEGER PRIMARY KEY AUTOINCREMENT, category_name VARCHAR(50) UNIQUE)"
   );
+
+
+  // Habib: J'ai ajouté les mock-datas de Tatiana dans la base de données pour conserver le même modèle qu'elle a utilisé en front
+
   db.run(
-    "INSERT INTO anime (nom, synopsis, auteur, genre, type, date_ajout, date_sorti, duree, status) VALUES (?,?,?,?,?,?,?,?,?)",
-    "Hugo",
-    "Hugo",
-    "Hugo",
-    "Hugo",
-    "Hugo",
-    "Hugo",
-    "Hugo",
-    "Hugo",
-    "Hugo"
+    "CREATE TABLE IF NOT EXISTS games (game_id INTEGER PRIMARY KEY AUTOINCREMENT, game_name VARCHAR(50) UNIQUE, game_img VARCHAR(200), game_resume VARCHAR(500), game_commentary VARCHAR(300), game_note INTEGER , category_name, FOREIGN KEY (category_name) REFERENCES categories(category_name))"
   );
+
+  db.run(
+    "INSERT INTO games (game_name, game_img, game_resume, game_commentary, game_note, category_name) VALUES (?,?,?,?,?,?)",
+    "Super Smash Bros : Ultimate",
+    "images/SSBU.png",
+    "Super Smash Bros Ultimate is a Fighting game on Switch. This Ultimate opus brings together all the fighters already appeared in the history of Super Smash Bros. 75 playable characters will be available by counting the trainer, as well as all his Pokémon...",
+    "I feel the same way as with Melee during my childhood. The game is complete, beautiful, funny addictive and fun to play. The OST is masterful, the game modes are legion, the characters are all there, and just the game out there is already announced a new! In short, whether you are a neophyte or a regular, go for it because in both cases you will not regret your purchase. This is THE Switch game of the year.",
+    16.2,
+    "Fighting"
+  );
+
+  db.run(
+    "INSERT INTO games (game_name, game_img, game_resume, game_commentary, game_note, category_name) VALUES (?,?,?,?,?,?)",
+    "The Last of Us",
+    "images/TLOU.jpg",
+    "The survival action The Last of Us on PS3 follows Joel and Ellie across the United States. The two will have to help each other to survive a mysterious plague. Nature begins to appropriate the abandoned cities and the few survivors kill each other to recover the little food and weapons still present.",
+    "The Last of Us is a must have on PS3. Innovative, the atmosphere is superb. Endearing characters, a narrative very well built. The stress is at the rendezvous knowing that we must manage its ammunition sparingly.",
+    16.8,
+    "Action"
+  );
+
+  db.run(
+    "INSERT INTO games (game_name, game_img, game_resume, game_commentary, game_note, category_name) VALUES (?,?,?,?,?,?)",
+    "League of Legends",
+    "images/LOL.jpg",
+    "League of Legends is a MOBA, a multiplayer online battle arena. In the classic mode, two teams of five players compete in games that last on average between 40 minutes and the goal is to destroy the enemy base. Playing in a hero-fantasy universe, each player embodies a different champion, with unique abilities, which he chooses at the beginning of the game. Random modes are also present, as well as seasonal events that bring a breath of novelty.",
+    "A very good game in F2P, everything is graphic, strategy, number of characters ... But unfortunately, the experience is not always happy, the fault of an absolutely disastrous community: in addition to insults and intolerance it is forbidden to be a beginner to this game ... It reminds me of a well-known MMO but this is another story.",
+    15.7,
+    "MOBA"
+  );
+
+  db.run("INSERT INTO categories (category_name) VALUES (?)", "Action");
+  db.run("INSERT INTO categories (category_name) VALUES (?)", "Fighting");
+  db.run("INSERT INTO categories (category_name) VALUES (?)", "RPG");
+  db.run("INSERT INTO categories (category_name) VALUES (?)", "Arcade");
+  db.run("INSERT INTO categories (category_name) VALUES (?)", "MOBA");
+  db.run("INSERT INTO categories (category_name) VALUES (?)", "MMORPG");
+  db.run("INSERT INTO categories (category_name) VALUES (?)", "FPS");
 });
 
-app.get("/", function(request, response) {
-  db.all("SELECT * FROM anime", function(error, data) {
+app.get("/", function (request, response) {
+  db.all("SELECT * FROM games", function (error, data) {
     response.send(data);
+    console.log("This is l'Acceuil");
     if (!error) console.log(data);
     else console.log(error);
   });
 });
 
-app.listen(3000, function() {
-  console.log("il à pas dis bonjour");
+app.get("/categories", function (request, response) {
+  db.all("SELECT * FROM categories", function (error, data) {
+    response.send(data);
+    console.log("This is all the different categories");
+    if (!error) console.log(data);
+    else console.log(error);
+  });
+
+});
+
+app.get("/Action", function (request, response) {
+  db.all("SELECT game_name FROM games WHERE category_name = 'Action' ", function (error, data) {
+    response.send(data);
+    console.log("This is all the Action games");
+    if (!error) console.log(data);
+    else console.log(error);
+  });
+});
+
+app.get("/Fighting", function (request, response) {
+  db.all("SELECT * FROM games WHERE category_name = 'Fighting' ", function (error, data) {
+    response.send(data);
+    console.log("This is all the Fighting games");
+    if (!error) console.log(data);
+    else console.log(error);
+  });
+});
+
+app.get("/RPG", function (request, response) {
+  db.all("SELECT * FROM games WHERE category_name = 'RPG' ", function (error, data) {
+    response.send(data);
+    console.log("This is all the RPG games");
+    if (!error) console.log(data);
+    else console.log(error);
+  });
+});
+
+app.get("/Arcade", function (request, response) {
+  db.all("SELECT * FROM games WHERE category_name = 'Arcade' ", function (error, data) {
+    response.send(data);
+    console.log("This is all the Arcade games");
+    if (!error) console.log(data);
+    else console.log(error);
+  });
+});
+
+app.get("/MOBA", function (request, response) {
+  db.all("SELECT * FROM games WHERE category_name = 'MOBA' ", function (error, data) {
+    response.send(data);
+    console.log("This is all the MOBA games");
+    if (!error) console.log(data);
+    else console.log(error);
+  });
+});
+
+app.get("/MMORPG", function (request, response) {
+  db.all("SELECT * FROM games WHERE category_name = 'MMORPG' ", function (error, data) {
+    response.send(data);
+    console.log("This is all the MMORPG games");
+    if (!error) console.log(data);
+    else console.log(error);
+  });
+});
+
+app.get("/FPS", function (request, response) {
+  db.all("SELECT * FROM games WHERE category_name = 'FPS' ", function (error, data) {
+    response.send(data);
+    console.log("This is all the FPS games");
+    if (!error) console.log(data);
+    else console.log(error);
+  });
+});
+
+// POST
+
+app.post("/", function (req, res) {
+  console.log(req.body.game_name);
+  var game = req.body;
+  db.run(
+    "INSERT INTO games (game_name, game_img, game_resume, game_commentary, game_note) VALUES (?,?,?,?,?)",
+    game.game_name,
+    game.game_img,
+    game.game_resume,
+    game.game_commentary,
+    game.game_note
+  );
+});
+
+
+app.listen(3000, function () {
+  console.log("Il a pas dis bonjour (Listening at port 3000)");
 });
--- front.js
+++ front.js
@@ -1,4 +1,112 @@
-$.get("http://localhost:3000/", function(res) {
-  var database = res;
-  console.log(database);
+$.get("http://localhost:3000/", function (res) {
+  var db = res;
+  make_item(db);
 });
+
+
+var gamesOfTheMoment = [
+
+  { name: 'Super Smash Bros : Ultimate', img: 'images/SSBU.png', synopsis: "Super Smash Bros Ultimate is a fighting game on Switch. This Ultimate opus brings together all the fighters already appeared in the history of Super Smash Bros. 75 playable characters will be available by counting the trainer, as well as all his Pokémon...", commentary: "I feel the same way as with Melee during my childhood. The game is complete, beautiful, funny addictive and fun to play. The OST is masterful, the game modes are legion, the characters are all there, and just the game out there is already announced a new! In short, whether you are a neophyte or a regular, go for it because in both cases you will not regret your purchase. This is THE Switch game of the year.", note: 16.2, type: 'Type : Combat' },
+  { name: 'The Last of Us', img: 'images/TLOU.jpg', synopsis: "The survival action The Last of Us on PS3 follows Joel and Ellie across the United States. The two will have to help each other to survive a mysterious plague. Nature begins to appropriate the abandoned cities and the few survivors kill each other to recover the little food and weapons still present.", commentary: "The Last of Us is a must have on PS3. Innovative, the atmosphere is superb. Endearing characters, a narrative very well built. The stress is at the rendezvous knowing that we must manage its ammunition sparingly.", note: 18.7, type: 'Type : Action' },
+  { name: 'League of Legends', img: 'images/LOL.jpg', synopsis: "League of Legends is a MOBA, a multiplayer online battle arena. In the classic mode, two teams of five players compete in games that last on average between 40 minutes and the goal is to destroy the enemy base. Playing in a hero-fantasy universe, each player embodies a different champion, with unique abilities, which he chooses at the beginning of the game. Random modes are also present, as well as seasonal events that bring a breath of novelty.", commentary: "A very good game in F2P, everything is graphic, strategy, number of characters ... But unfortunately, the experience is not always happy, the fault of an absolutely disastrous community: in addition to insults and intolerance it is forbidden to be a beginner to this game ... It reminds me of a well-known MMO but this is another story.", note: 15.7, type: 'Type : MOBA' },
+
+];
+
+  var container = document.getElementById('container');
+
+  var header = document.createElement('header');
+  var title = document.createElement('h1');
+  title.setAttribute('id', 'title');
+  title.textContent = "GameStore";
+
+  var paragraphe = document.createElement('p');
+  paragraphe.setAttribute('class', 'paragraphe');
+  paragraphe.innerHTML = "Do business on games, extensions, pre-orders and more. <br> Even more discounts for GameStore Plus subscribers.";
+
+
+  var logo = document.createElement('img');
+  logo.setAttribute('id', 'logo');
+  logo.setAttribute('src', 'images/gamestore_logo.png');
+
+
+  var basket = document.createElement('img');
+  basket.setAttribute('id', 'basket');
+  basket.setAttribute('src', 'images/shopping_basket.png');
+  ;
+
+  var namelist = ["Home", "Games", "GameStore Plus", "Store", "Help"];
+  var ul = document.createElement('ul');
+
+  container.appendChild(ul);
+  ul.appendChild(logo);
+  ul.appendChild(basket);
+  for (let i = 0; i < 5; i++) {
+    var list = document.createElement('li');
+    var a = document.createElement('a');
+    a.setAttribute('href', '#')
+    list.setAttribute('class', 'list');
+    a.textContent = namelist[i];
+    ul.appendChild(list);
+    list.appendChild(a);
+
+
+    if (i == 1) {
+      list.setAttribute('id', 'scroll_menu');
+    }
+
+  }
+
+  console.log(namelist);
+
+
+  basket.addEventListener('click', function () {
+    btnBasket();
+  });
+
+  container.appendChild(header);
+  
+  
+  ul.appendChild(list);
+  header.appendChild(title);
+  header.appendChild(paragraphe);
+  
+
+
+  var basket = document.createElement('img');
+  basket.setAttribute('id', 'basket');
+  basket.setAttribute('src', 'images/shopping_basket.png');
+  ;
+
+
+  var gotm = document.createElement('div');
+  gotm.setAttribute('id', 'gotm');
+
+  container.appendChild(gotm);
+
+  function make_item(db) {
+  db.forEach(function (game) {
+    console.log(game);
+    var games = document.createElement('div');
+    games.setAttribute('class', "games_of_the_moment");
+    var fiche = document.createElement('div');
+    fiche.setAttribute('class', "fiche");
+    var titlesGames = document.createElement('h1');
+    titlesGames.setAttribute('class', 'titlesGOTM');
+    titlesGames.textContent = game.game_name;
+    var imagesGames = document.createElement('img');
+    imagesGames.setAttribute('src', game.game_img);
+    var synopsisGames = document.createElement('p');
+    synopsisGames.setAttribute('class', 'synopsis');
+    synopsisGames.textContent = game.game_resume;
+
+
+    container.appendChild(games);
+    games.appendChild(titlesGames);
+    games.appendChild(fiche);
+    fiche.appendChild(imagesGames);
+    fiche.appendChild(synopsisGames);
+  
+    gotm.appendChild(games);
+
+  });
+  }
