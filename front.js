ef3e1fc1cc72bfd1c4ac7fbd1d7e1cf3b1a9fdc0
Tatiana <choisy.tatiana@gmail.com>
2019-03-13 09:51:19 +0100

    Ajout d'une page de bibliothèque de jeux vidéos

--- front.js
+++ front.js
@@ -39,30 +39,30 @@ make_item(db);
 });
 */
 var container = document.getElementById("container");
-
 var header = document.createElement("header");
+
 var title = document.createElement("h1");
-title.setAttribute("id", "title");
-title.textContent = "GameStore";
+    title.setAttribute("id", "title");
+    title.textContent = "GameStore";
 
 var paragraphe = document.createElement("p");
-paragraphe.setAttribute("class", "paragraphe");
-paragraphe.innerHTML =
-"Do business on games, extensions, pre-orders and more. <br> Even more discounts for GameStore Plus subscribers.";
+    paragraphe.setAttribute("class", "paragraphe");
+    paragraphe.innerHTML = "Make good deals on games, extensions, pre-orders and more <br> Even more discounts for GameStore Plus subscribers";
 
 var logo = document.createElement("img");
-logo.setAttribute("id", "logo");
-logo.setAttribute("src", "images/gamestore_logo.png");
+    logo.setAttribute("id", "logo");
+    logo.setAttribute("src", "images/gamestore_logo.png");
 
 var basket = document.createElement("img");
-basket.setAttribute("id", "basket");
-basket.setAttribute("src", "images/shopping_basket.png");
-var namelist = ["Home", "Games", "GameStore Plus", "Store", "Help"];
+    basket.setAttribute("id", "basket");
+    basket.setAttribute("src", "images/shopping_basket.png");
+var namelist = ["Home", "New", "Promotions", "Store", "GameStore Plus"];
 var ul = document.createElement("ul");
 
 container.appendChild(ul);
 ul.appendChild(logo);
 ul.appendChild(basket);
+
 for (let i = 0; i < 5; i++) {
 var list = document.createElement("li");
 var a = document.createElement("a");
@@ -72,7 +72,7 @@ a.textContent = namelist[i];
 ul.appendChild(list);
 list.appendChild(a);
 
-if (i == 1) {
+if (i == 3) {
 list.setAttribute("id", "scroll_menu");
 }
 }
@@ -90,38 +90,44 @@ header.appendChild(title);
 header.appendChild(paragraphe);
 
 var basket = document.createElement("img");
-basket.setAttribute("id", "basket");
-basket.setAttribute("src", "images/shopping_basket.png");
+    basket.setAttribute("id", "basket");
+    basket.setAttribute("src", "images/shopping_basket.png");
 var gotm = document.createElement("div");
-gotm.setAttribute("id", "gotm");
+    gotm.setAttribute("id", "gotm");
 
 container.appendChild(gotm);
 
 var games_of_the_moment = document.createElement("h1");
-games_of_the_moment.setAttribute("id", "title_gotm");
-games_of_the_moment.textContent = "Games Of The Moment";
+    games_of_the_moment.setAttribute("id", "title_gotm");
+    games_of_the_moment.textContent = "Games Of The Moment";
 
 var div_gotm = document.createElement("div");
-div_gotm.setAttribute("id", "div_gotm");
+    div_gotm.setAttribute("id", "div_gotm");
 gotm.appendChild(games_of_the_moment);
 gotm.appendChild(div_gotm);
+
+var footer = document.createElement('footer');
+container.appendChild(footer);
 
 function make_gotm(db) {
 db.forEach(function(game) {
 var games = document.createElement("div");
-games.setAttribute("class", "games_of_the_moment");
+    games.setAttribute("class", "games_of_the_moment");
 var fiche = document.createElement("div");
-fiche.setAttribute("class", "fiche");
+    fiche.setAttribute("class", "fiche");
 var titlesGames = document.createElement("h1");
-titlesGames.setAttribute("class", "titlesGOTM");
-titlesGames.textContent = game.game_name;
+    titlesGames.setAttribute("class", "titlesGOTM");
+    titlesGames.textContent = game.game_name;
 var divImages = document.createElement('div');
-divImages.setAttribute("class", "div_images");
+    divImages.setAttribute("class", "div_images");
 var imagesGames = document.createElement("img");
-imagesGames.setAttribute("src", game.game_img);
+    imagesGames.setAttribute("src", game.game_img);
 var synopsisGames = document.createElement("p");
-synopsisGames.setAttribute("class", "synopsis");
-synopsisGames.textContent = game.game_resume;
+    synopsisGames.setAttribute("class", "synopsis");
+    synopsisGames.textContent = game.game_resume;
+var notesGames = document.createElement("p");
+    notesGames.setAttribute("class", "note");
+    notesGames.textContent = game.game_note;
 
 container.appendChild(games);
 games.appendChild(fiche);
@@ -129,29 +135,33 @@ fiche.appendChild(titlesGames);
 fiche.appendChild(divImages);
 fiche.appendChild(imagesGames);
 divImages.appendChild(synopsisGames);
+divImages.appendChild(notesGames);
 div_gotm.appendChild(games);
+    
 });
 }
 
 function make_item(db) {
 db.forEach(function(game) {
 var games = document.createElement("div");
-games.setAttribute("class", "games_of_the_moment");
+    games.setAttribute("class", "games_of_the_moment");
 var fiche = document.createElement("div");
-fiche.setAttribute("class", "fiche");
+    fiche.setAttribute("class", "fiche");
 var titlesGames = document.createElement("h1");
-titlesGames.setAttribute("class", "titlesGOTM");
-titlesGames.textContent = game.game_name;
+    titlesGames.setAttribute("class", "titlesGOTM");
+    titlesGames.textContent = game.game_name;
 var imagesGames = document.createElement("img");
-imagesGames.setAttribute("src", game.game_img);
+    imagesGames.setAttribute("src", game.game_img);
 var synopsisGames = document.createElement("p");
-synopsisGames.setAttribute("class", "synopsis");
-synopsisGames.textContent = game.game_resume;
+    synopsisGames.setAttribute("class", "synopsis");
+    synopsisGames.textContent = game.game_resume;
+    
 
 container.appendChild(games);
 games.appendChild(fiche);
 fiche.appendChild(titlesGames);
 fiche.appendChild(imagesGames);
 fiche.appendChild(synopsisGames);
+    
 });
 }
--- games.js
+++ games.js
@@ -1,69 +1,168 @@
 $.get("http://localhost:3000/", function(res) {
-    var db = res;
-    addItems(db);
+var db = res;
+make_items(db);
+});
+/*
+$.get("http://localhost:3000/categories", function(res) {
+var db = res;
+make_item(db);
 });
 
+$.get("http://localhost:3000/Action", function(res) {
+var db = res;
+make_item(db);
+});
+
+$.get("http://localhost:3000/Fighting", function(res) {
+var db = res;
+make_item(db);
+});
+
+$.get("http://localhost:3000/RPG", function(res) {
+var db = res;
+make_item(db);
+});
+
+$.get("http://localhost:3000/Arcade", function(res) {
+var db = res;
+make_item(db);
+});
+
+$.get("http://localhost:3000/MOBA", function(res) {
+var db = res;
+make_item(db);
+});
+
+$.get("http://localhost:3000/FPS", function(res) {
+var db = res;
+make_item(db);
+});
+*/
 var container = document.getElementById("container");
+var header = document.createElement("header");
 
+var title = document.createElement("h1");
+    title.setAttribute("id", "title");
+    title.textContent = "GameStore";
 
-var games = document.createElement('div');
-games.id = "games";
-        
-function addItems(db) {
-    db.forEach(function(game) {
+var paragraphe = document.createElement("p");
+    paragraphe.setAttribute("class", "paragraphe");
+    paragraphe.innerHTML = "Do business on games, extensions, pre-orders and more <br> Even more discounts for GameStore Plus subscribers";
 
-    var blockGames = document.createElement('div');
-        blockGames.id = "block_games";
+var logo = document.createElement("img");
+    logo.setAttribute("id", "logo");
+    logo.setAttribute("src", "images/gamestore_logo.png");
 
-    var gamesImages = document.createElement('div');
-        gamesImages.id = "game_images";
+var basket = document.createElement("img");
+    basket.setAttribute("id", "basket");
+    basket.setAttribute("src", "images/shopping_basket.png");
+var namelist = ["Home", "New", "Promotions", "Store", "GameStore Plus"];
+var ul = document.createElement("ul");
 
-    var image = document.createElement('img');
-        image.src = game.game_img;
+container.appendChild(ul);
+ul.appendChild(logo);
+ul.appendChild(basket);
 
-    var synopsisGames = document.createElement("p");
-        synopsisGames.setAttribute("class", "synopsis");
-        synopsisGames.textContent = game.game_resume;
+for (let i = 0; i < 5; i++) {
+var list = document.createElement("li");
+var a = document.createElement("a");
+a.setAttribute("href", "#");
+list.setAttribute("class", "list");
+a.textContent = namelist[i];
+ul.appendChild(list);
+list.appendChild(a);
 
-    var commentaryGames = document.createElement("p");
-        commentaryGames.setAttribute("class", "resume");
-        commentaryGames.textContent = game.game_commentary;
-        
-    var noteGames = document.createElement("h6")
-        noteGames.setAttribute("class", "note");
-
-        // var priceGames = document.createElement("")
-        //priceGames.id = game.game_price;
-
-        var btn = document.createElement('button');
-        var textBtn = document.createTextNode('ADD TO BASKET');
-        btn.setAttribute('id', 'buy_button');
-
-        function btnBuy() {
-            document.getElementById('basket').style.color = 'darkred';
-            console.log("Je mets le jeu à mon panier");
-          }
-          btn.addEventListener('click', function() {
-              btnBuy();
-              
-               
-          });
-        
-        container.appendChild(games);
-        games.appendChild(blockGames);
-        blockGames.appendChild(gamesImages);
-        gamesImages.appendChild(image);
-        blockGames.appendChild(synopsisGames);
-        blockGames.appendChild(commentaryGames);
-        blockGames.appendChild(noteGames);
-    
-
-        
-        
-
-});
-
-
-
+if (i == 3) {
+list.setAttribute("id", "scroll_menu");
+}
 }
 
+console.log(namelist);
+
+basket.addEventListener("click", function() {
+btnBasket();
+});
+
+container.appendChild(header);
+
+ul.appendChild(list);
+
+var basket = document.createElement("img");
+    basket.setAttribute("id", "basket");
+    basket.setAttribute("src", "images/shopping_basket.png");
+
+
+
+var footer = document.createElement('footer');
+container.appendChild(footer);
+
+function make_items(db) {
+    db.forEach(function (game) {
+var containerGames = document.createElement("div");
+    containerGames.setAttribute('id', 'container_games');
+var games = document.createElement("div");
+    games.setAttribute("class", "games");
+var images = document.createElement("img");
+    images.setAttribute("src", game.game_img);
+var titles = document.createElement("h1");
+    titles.setAttribute("class", "titles");
+    titles.textContent = game.game_name;
+var synopsis = document.createElement("p");
+    synopsis.setAttribute("class", "synopsis");
+    synopsis.textContent = game.game_resume;
+var notes = document.createElement("p");
+    notes.setAttribute("class", "note");
+    notes.textContent = game.game_note;
+
+        
+        container.appendChild(header);
+header.appendChild(containerGames);
+containerGames.appendChild(games);
+games.appendChild(images);
+games.appendChild(titles);
+games.appendChild(synopsis);
+games.appendChild(notes);
+    
+});
+}
+
+function make_items(db) {
+    db.forEach(function(game) {
+    var games = document.createElement("div");
+        games.setAttribute("class", "games");
+        var divImages = document.createElement('div');
+        divImages.setAttribute("class", "images");
+    var images = document.createElement("img");
+        images.setAttribute("src", game.game_img);
+    var titles = document.createElement("h1");
+        titles.setAttribute("class", "titles");
+        titles.textContent = game.game_name;
+    var synopsis = document.createElement("p");
+        synopsis.setAttribute("class", "synopsis");
+        synopsis.textContent = game.game_resume;
+    var notes = document.createElement("p");
+        notes.setAttribute("class", "note");
+        notes.textContent = game.game_note;
+    
+       
+
+    header.appendChild(games);
+    games.appendChild(images);
+    games.appendChild(titles);
+    games.appendChild(synopsis);
+    games.appendChild(notes);
+        
+    });
+}
+    
+var basketProduct = document.getElementById('basket');
+
+var basket = document.createElement('i');
+basket.setAttribute('class', 'fas fa-shopping-bag');
+
+container.appendChild(basketProduct);
+basketProduct.appendChild(basket);
+
+basket.addEventListener('click', function() {
+  btnBasket();
+});
